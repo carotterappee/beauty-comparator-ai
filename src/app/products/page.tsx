975bd5b8792fe63a49f8d2867d6fb504be78f240
carotterappee <caroline13.loiseau@gmail.com>
2025-10-05 20:10:00 +0000

    feat: add /products page

---
+++ src/app/products/page.tsx
@@ -0,0 +1,9 @@
+// src/app/products/page.tsx
+export default function ProductsPage() {
+  return (
+    <main className="p-10">
+      <h1 className="text-2xl font-bold">Products OK</h1>
+      <p className="text-gray-600">Cette page existe bien ✅</p>
+    </main>
+  );
+}
