953296b6a2f34e9edcc5832f9222c99cb69b4667
carotterappee <caroline13.loiseau@gmail.com>
2025-10-12 15:27:17 +0000

    fix: header div séparées pour logo et wordmark

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,21 +7,26 @@ export default function Header() {
   return (
     <header className="w-full">
       <div className="relative mx-auto max-w-7xl px-6 py-6">
+
         {/* Logo cliquable en haut-gauche */}
+        <div className="absolute left-6 top-0">
         <Link
           href="/"
           aria-label="Accueil"
-          className="absolute left-6 top-[-14px]"
-        >
+          className="absolute left-6 top-0"
+          >
           <VMBadgeSVG size={96} className="shrink-0" />
         </Link>
+        </div>
 
         {/* Wordmark centré (utilise <img> pour éviter le blocage SVG de next/image) */}
+        <div>
         <img
           src="/velvetmind/vm-wordmark.svg"  
           alt="VelvetMind Wordmark"
-          className="block mx-auto mt-[5rem] h-[130px] w-auto select-none"
+          className="block mx-auto mt-[5rem] h-[140px] w-auto select-none"
         />
+        </div>
       </div>
     </header>
   );
