154d8d32cc2ec81d28605b5238864c2e0c53f310
carotterappee <caroline13.loiseau@gmail.com>
2025-10-15 07:38:36 +0000

    fix(header): ajustement du logo dans LogoPin (position revue)

--- src/components/header/LogoPin.tsx
+++ src/components/header/LogoPin.tsx
@@ -5,13 +5,13 @@ import VMBadgeSVG from "@/components/VMBadgeSVG";
 
 export default function Header() {
   return (
-    <header className="relative w-full">
+    <header className="relative w-full  h-[360px] md:h-[400px] lg:h-[440px]">
 
       {/* Logo: sticks to the top-left of the header */}
       <Link
         href="/"
         aria-label="Accueil"
-        className="absolute left-6 top-0"
+        className="absolute left-6 top-[48px] md:top-[56px] lg:top-[64px]"
       >
         <VMBadgeSVG size={96} className="shrink-0" />
       </Link>
